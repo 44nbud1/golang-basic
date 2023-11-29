33ec779d4c3cd592c3f2d92bc69a1522a0d78143
Aan Budi Setiawan <setiawan.aanbudi@gmail.com>
2023-11-30 05:42:40 +0700

    perbandingan

---
+++ 1.basic/7_perbandingan.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("aan" == "aan") // perbandingan apakah sama ? , return bool
+	fmt.Println("aan" != "aan") // perbandingan apakah beda ?, return bool
+	fmt.Println(3 < 5)          // apakah kurang dari ?, return bool
+	fmt.Println(3 > 5)          // apakah lebih dari ?, return bool
+	fmt.Println(3 <= 5)         // apakah kurang dari atau sama dengan ?, return bool
+	fmt.Println(3 >= 5)         // apakah lebih dari atau sama dengan ? , return bool
+}
